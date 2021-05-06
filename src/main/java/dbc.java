bc44d425935c413ca6c89da30875752a7627bd79
JurreNagelWindesheim <74776180+JurreNagelWindesheim@users.noreply.github.com>
2021-05-06 11:48:52 +0200

    added catches and finally to try db

--- src/main/java/dbc.java
+++ src/main/java/dbc.java
@@ -3,26 +3,50 @@ import java.sql.*;
 public class dbc {
 
     public static void getConnection() throws SQLException {
+        // define conection and statement
+        Connection conn = null;
+        Statement stmt = null;
+
+        // db connection info
         String user = "root";
         String password = null;
         String db = "nerdygadgets";
 
         //create connection for a server installed in localhost
-        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/"+db, user, password)) {
+        try {
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost/" + db, user, password);
 
             // create a Statement
-            try (Statement stmt = conn.createStatement()) {
+            stmt = conn.createStatement();
 
-                //execute query
-                try (ResultSet rs = stmt.executeQuery("SELECT count(*) FROM cities")) {
+            //execute query
+            try (ResultSet rs = stmt.executeQuery("SELECT count(*) FROM cities")) {
+                //position result to first
+                rs.first();
 
-                    //position result to first
-                    rs.first();
-
-                    // loop trough result
-                    System.out.println("Aantal cities in db: "+rs.getString(1)); //result is "Hello World!"
-                }
+                // print result
+                System.out.println("Aantal cities in db: " + rs.getString(1));
             }
-        }
+        } catch (Exception e) {
+            // catch errors
+            System.err.println(e.getMessage());
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                // do nothing
+            } try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } //end finally try
+        } //end try
+        // Print message everything closed
+        System.out.println("SQL success, goodbye!");
     }
 }
